f776af3672a66c74241937c3bb05580c3f33a56e
Sparisoma Viridi <dudung@gmail.com>
2022-10-01 06:07:37 +0700

    Update and rename sturmquellensuche.html to sturmquellensuche.hs

---
+++ 2022/09/sturmquellensuche.hs
@@ -0,0 +1,8 @@
+main :: IO ()
+
+main = do
+  putStrLn "name Sturmquellensuche"
+  putStrLn "date 30-sep-2022"
+  putStrLn "usrs Sevi Nurafni, Hirofumi Notsu"
+  putStrLn "keys Welle, Wind, Sturmquelle, Wetterdaten"
+  putStrLn "meto SWE, FD, FEM, ML"
