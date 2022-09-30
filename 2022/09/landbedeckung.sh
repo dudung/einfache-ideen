b16f19e63ef1f6762acec310563a1945f79f9b26
Sparisoma Viridi <dudung@gmail.com>
2022-10-01 06:25:56 +0700

    Create landbedeckung.sh

---
+++ 2022/09/landbedeckung.sh
@@ -0,0 +1,10 @@
+main :: IO ()
+
+main = do
+  putStrLn "name Landbedeckung"
+  putStrLn "date 30-sep-2022"
+  putStrLn "usrs Faizal Rohmat, Winda Wijayasari"
+  putStrLn "keys Wasserdurchfluss, Niederschlagsdaten, Landbedeckung"
+  putStrLn "meto DTMC, MLRM"
+
+-- url https://onecompiler.com/haskell/3yhkuh4n3
