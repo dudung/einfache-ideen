18585209f14ad764f2e634cdc39aac39f928cb33
Sparisoma Viridi <dudung@gmail.com>
2022-10-14 05:20:50 +0700

    Create universitaetsverlag.hs

---
+++ 2022/10/universitaetsverlag.hs
@@ -0,0 +1,10 @@
+main :: IO ()
+
+main = do
+  putStrLn "name Universitätsverlag"
+  putStrLn "date 13-oct-2022"
+  putStrLn "usrs Edi Wahyu Sri Mulyono"
+  putStrLn "keys ITB-Verlag, Störung, Transformation"
+  putStrLn "meto ABM, SD"
+
+-- url https://onecompiler.com/haskell/3yjuy92af
